aad02e5f3d98784db79b44199f58e97dea45cc0a
Abhiram Nagadi <40509745+Abhiram13@users.noreply.github.com>
2023-01-28 10:35:17 +0530

    commented calayer

--- Core Data/ViewController.swift
+++ Core Data/ViewController.swift
@@ -16,22 +16,25 @@ class ViewController: UIViewController {
         
         subView.backgroundColor = .blue;
         subView.translatesAutoresizingMaskIntoConstraints = false;
-        subView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true;
-        subView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true;
+        subView.widthAnchor.constraint(equalToConstant: 40).isActive = true;
+        subView.heightAnchor.constraint(equalToConstant: 100).isActive = true;
         subView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true;
         subView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true;
+        
+        subView.transform = CGAffineTransform(rotationAngle: 90)
                 
-        let freeform = UIBezierPath();
-        let caLayer = CAShapeLayer();
-        
-        freeform.move(to: CGPoint(x: 200, y: 200));
-        freeform.addLine(to: CGPoint(x: 200, y: 400))
-        freeform.addLine(to: CGPoint(x: 150, y: 400))
-        freeform.addLine(to: CGPoint(x: 150, y: 200))
-        
-        caLayer.path = freeform.cgPath;
-        caLayer.fillColor = UIColor.red.cgColor;
-        
-        subView.layer.addSublayer(caLayer);
+//        let freeform = UIBezierPath();
+//        let caLayer = CAShapeLayer();
+//
+//        freeform.move(to: CGPoint(x: 200, y: 200));
+//        freeform.addLine(to: CGPoint(x: 200, y: 400))
+//        freeform.addLine(to: CGPoint(x: 150, y: 400))
+//        freeform.addLine(to: CGPoint(x: 150, y: 200))
+//        freeform.close();
+//
+//        caLayer.path = freeform.cgPath;
+//        caLayer.fillColor = UIColor.red.cgColor;
+//
+//        subView.layer.addSublayer(caLayer);
     }
 }
